f0e820a8d734eecf4f3785c526711673ab1d0371
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-15 08:48:46 +0300

    styled navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 function Navbar() {
     return (
-        <nav>
-            <div>
-                <h1>OneMoreEpisode</h1>
+        <nav className="flex flex-wrap justify-between bg-white shadow-md p-5 items-center">
+            <div className="mb-2 sm:mb-0">
+                <h1 className="font-bold text-2xl">OneMoreEpisode</h1>
             </div>
-            <div>
-                <input type="text" placeholder="Search..."/>
-                <button>Search</button>
-                <button>Categories</button>
+            <div className="flex flex-wrap gap-2">
+                <input className="border w-40 sm:w-48 md:w-56 lg:w-64 p-2 border-stone-300 rounded-2xl text-sm font-thin" type="text" placeholder="Search..."/>
+                <button className="px-3 py-1 bg-black text-white rounded-md">Search</button>
+                <button className="px-3 py-1 bg-black text-white rounded-md">Categories</button>
             </div>
         </nav>
     )
