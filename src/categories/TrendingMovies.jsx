2e2fc77343f0ff62dd5f1c41db1fa1c333cad810
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-15 19:28:01 +0300

    style: fixed styling on the movie list

--- src/categories/TrendingMovies.jsx
+++ src/categories/TrendingMovies.jsx
@@ -12,20 +12,20 @@ function TrendingMovies() {
     if (isError) return <p>Error: {error.message}</p>
 
     return (
-      <div >
-        <h2>Trending</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 p-6">
-          {data.results.slice(0, 15).map((movies) => (
-            <div className="bg-white shadow-md rounded-lg" key={movies.id}>
+      <div>
+        <h2 className="text-xl font-semibold p-3">Trending</h2>
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-6">
+          {data.results.slice(0, 18).map((movies) => (
+            <div className="bg-white shadow-md rounded-lg flex flex-col h-full" key={movies.id}>
               <img className="w-full h-48 object-cover" src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}/>
-              <div className="p-4">
+              <div className="p-4 flex flex-col justify-between flex-grow">
                 <h4 className="text-lg font-bold">{movies.title}</h4>
-                <div className="flex">
-                  <p>{movies.vote_average ? movies.vote_average.toFixed(1) : "No rating yet"}</p>
-                  <p>{movies.release_date?.slice(0, 4)}</p>
-                  <p>{movies.media_type}</p>
+                <div className="flex flex-wrap text-gray-500 font-light text-sm">
+                  <p className="m-1">{movies.vote_average ? movies.vote_average.toFixed(1) : "No rating yet"}</p>
+                  <p className="m-1">{movies.release_date?.slice(0, 4)}</p>
+                  <p className="m-1">{movies.media_type}</p>
                 </div>
-                <button>Watch Now</button>
+                <button className="bg-black text-white text-sm p-2 rounded-md hover:bg-gray-700 hover:cursor-pointer bottom-2">Watch Now</button>
               </div>
             </div>
 ))}
