69c432ff88fbfb99a5f473eebeadf2370993ca67
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-15 08:11:06 +0300

    feat: built navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <h1>OneMoreEpisode</h1>
+            </div>
+            <div>
+                <input type="text" placeholder="Search..."/>
+                <button>Search</button>
+                <button>Categories</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
