9ac9d8ac545df3bdce23b9d2fb677d0587c2a3ab
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-15 16:28:53 +0300

    created moviecard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+function MovieCard() {
+    return (
+        <div>
+            <img></img>
+            <h4>Movie title</h4>
+            <p>Year</p>
+            <button>Watch Now</button>
+        </div>
+    )
+}
+
+export default MovieCard;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import MovieCard from "../components/MovieCard";
 import Movielist from "../components/Movielist";
 import Navbar from "../components/Navbar";
 
@@ -8,7 +9,7 @@ function Homepage() {
         <div className="bg-stone-200">
         <Navbar/>
         <Hero/>
-        <Movielist/>
+        <MovieCard/>
         <Footer/>
         </div>
     )
