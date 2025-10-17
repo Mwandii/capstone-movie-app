0e21996b0a780c8cb64f9800b980e6142ebaadf3
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-17 13:02:28 +0300

    style: enhanced styling on genre page

--- src/pages/GenrePage.jsx
+++ src/pages/GenrePage.jsx
@@ -14,21 +14,21 @@ function GenrePage() {
     if (isError) return <p>Error: {error.message}</p>
 
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
         <Navbar/>
-<div>
+<div className="flex-grow">
             <h1 className="font-semibold text-3xl m-2">Genres</h1>
         <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-6">
             
         {data.genres.map((genres) => (
-            <div key={genres.id} className="bg-gray-300 text-gray-800 rounded-xl p-10  shadow-md transform transition-all duration-300 hover:-translate-y-2 hover:shadow-lg  hover:shadow-gray-500 hover:cursor-pointer">
-                <h1 className="font-semibold text-2xl ">{genres.name}</h1>
+            <div key={genres.id} className="bg-gray-300 text-gray-800 rounded-xl p-6  shadow-md transform transition-all duration-300 hover:-translate-y-2 hover:shadow-lg  hover:shadow-gray-500 hover:cursor-pointer flex items-center justify-center text-center">
+                <h1 className="font-semibold text-xl break-words whitespace-normal leading-tight">{genres.name}</h1>
             </div>
         ))}
         </div>
         </div>
         <Footer/>
-        </>
+        </div>
     )
 }
 
