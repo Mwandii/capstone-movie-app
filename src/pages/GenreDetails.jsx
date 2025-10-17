1314f3754bd34ed3ddab47d7165d32e3b924542b
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-17 14:09:59 +0300

    improved ui

--- src/pages/GenreDetails.jsx
+++ src/pages/GenreDetails.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { Link, useParams } from "react-router-dom";
 import { fetchGenreDetails } from "../Services/api";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function GenreDetails() {
 
@@ -15,8 +17,9 @@ function GenreDetails() {
     if (isError) return <p>Error: {error.message}</p>
 
     return (
-         <div>
-        <h2 className="text-xl font-semibold p-3">All Movies</h2>
+         <div className="bg-stone-200">
+            <Navbar/>
+        <h2 className="text-xl font-semibold p-3">Movies By Genre</h2>
         <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-6">
           {data.results.map((movies) => (
             <div className="bg-white shadow-md rounded-lg flex flex-col h-full" key={movies.id}>
@@ -35,6 +38,7 @@ function GenreDetails() {
             </div>
 ))}
         </div>
+        <Footer/>
       </div>
     )
 }
