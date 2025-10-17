68ae093bf565f68d1fa8a10fa17c58c6ac8a299e
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-17 12:54:15 +0300

    rendered genre page to display real data

--- src/pages/GenrePage.jsx
+++ src/pages/GenrePage.jsx
@@ -1,23 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { fetchMovieGenres } from "../Services/api";
 
 function GenrePage() {
+
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ['genrePage'],
+        queryFn: fetchMovieGenres
+    })
+
+    if (isLoading) return <p>Loading...</p>
+    if (isError) return <p>Error: {error.message}</p>
+
     return (
         <>
         <Navbar/>
-        <div>
+<div>
             <h1 className="font-semibold text-3xl m-2">Genres</h1>
-        <div className="relative grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-6">
-            <div class="bg-gray-300 text-gray-800 rounded-xl p-10 shadow-md transform transition-all duration-300 hover:-translate-y-2 hover:shadow-lg  hover:shadow-gray-500 hover:cursor-pointer">
-                <h1 className="font-semibold text-2xl">Name</h1>
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-6">
+            
+        {data.genres.map((genres) => (
+            <div key={genres.id} className="bg-gray-300 text-gray-800 rounded-xl p-10  shadow-md transform transition-all duration-300 hover:-translate-y-2 hover:shadow-lg  hover:shadow-gray-500 hover:cursor-pointer">
+                <h1 className="font-semibold text-2xl ">{genres.name}</h1>
             </div>
-            <div className="bg-white shadow-md rounded-lg flex flex-col h-full p-10">
-                <h1 className="font-semibold text-2xl">Name</h1>
-            </div>
-            <div className="bg-white shadow-md rounded-lg flex flex-col h-full p-10">
-                <h1 className="font-semibold text-2xl">Name</h1>
-            </div>
-            </div>
+        ))}
+        </div>
         </div>
         <Footer/>
         </>
